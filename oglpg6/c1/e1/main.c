88c0cbeb1fc80f06d13452ed640cf51a0670194d
Paul Liu <rocliulp@gmail.com>
2012-08-12 16:05:38 +0800

    first commit

---
+++ oglpg6/c1/e1/main.c
@@ -0,0 +1,18 @@
+#include <windows.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+int main (int argc, char * argv[]) {
+  glClearColor (0.0, 0.0, 0.0, 0.0);
+  glClear (GL_COLOR_BUFFER_BIT);
+  glColor3f (1.0, 1.0, 1.0);
+  glOrtho (0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+  glBegin (GL_POLYGON);
+    glVertex3f (0.25, 0.25, 0.0);
+    glVertex3f (0.75, 0.25, 0.0);
+    glVertex3f (0.75, 0.75, 0.0);
+    glVertex3f (0.25, 0.75, 0.0);
+  glEnd ();
+  glFlush ();
+  return 0;
+}
